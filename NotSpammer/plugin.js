1ad4e5ea5cdb78bc3024ee5fea3caedd97def004
ItzOnlyAnimal <ItzOnlyAnimal@users.noreply.github.com>
2022-06-08 23:41:52 +0000

    deploy: 5b525a0a0365cee38a432ae4298c792dd0e5bf84

---
+++ NotSpammer/plugin.js
@@ -0,0 +1,1 @@
+(function(o,n){"use strict";const c=o.findByProps("isSpam"),a=n.instead("isSpam",c,(s,t)=>!1);var r=s=>({onLoad(){},onUnload(){a()}});return r})(cumcord.modules.webpack,cumcord.patcher);
